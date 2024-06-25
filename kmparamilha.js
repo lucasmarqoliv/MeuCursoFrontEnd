e06206bdad239dbba7ffa7db249cf70216ca85f8
Lucas Marques de Oliveira <167311748+lucasmarqoliv@users.noreply.github.com>
2024-06-25 16:55:24 -0300

    ok

---
+++ kmparamilha.js
@@ -0,0 +1,8 @@
+// FUNÇÃO PARA CONVERTER KM PARA MILHAS 
+
+function converterKmParaMilhas(valorKM) {
+    let milhas = valorKM * 0.62137
+    console.log('a conversão de', valorKM, 'KM para milhas é igual a:', milhas)
+    return valorKM
+}
+converterKmParaMilhas(100)
