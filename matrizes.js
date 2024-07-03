8687af6c985c16872e737d431b2205835d50d500
Lucas Marques de Oliveira <167311748+lucasmarqoliv@users.noreply.github.com>
2024-07-03 16:00:19 -0300

    ok

---
+++ matrizes.js
@@ -0,0 +1,15 @@
+// PRATICANDO MATRIZES COM VARIAVEIS, E EM FUNÇÕES
+
+let notas = [10, 9, 5, 7, 8]
+console.log(notas[0]) // VAI RETORNAR O PRIMEIRO ITEM DA LISTA.
+
+
+mesesDoAno = [1, 2, 3]
+let mes = mesesDoAno[1]
+console.log(mes)// VAI RETORNAR O PRIMEIRO ITEM DA LISTA QUE CONTEM NA VAR MESESDOANO
+
+
+function numerosAleatorios(numeros){
+    return numeros[2]
+}
+console.log(numerosAleatorios([1,2,3,4,5,6,])) // VAI RETORNAR O TERCEIRO ITEM DA MATRIZ NUMEROS
