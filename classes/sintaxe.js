8e84d4b5465c679a8f8bc4ccbef68737efff6122
Lucas Marques de Oliveira <167311748+lucasmarqoliv@users.noreply.github.com>
2024-08-12 17:45:39 -0300

    ok

--- classes/sintaxe.js
+++ classes/sintaxe.js
@@ -1,4 +1,5 @@
 
+// São fábricas para criar objetos de acordo com as especificações que voce fornecer.
 // classes = permitem agrupar variaveis e funções que descrevem uma entidade. ex: produto, usuario, receita, etc.
 // nomenclaturas dentro da classe: variaveis = propriedades. funções = metodos.
 class PrimeiraLetraMaiuscula { // boa pratica usar primeira letra maiuscula.
