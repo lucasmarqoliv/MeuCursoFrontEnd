7e39c63d9d72daae64567f29c8de742bce838a4c
Lucas Marques de Oliveira <167311748+lucasmarqoliv@users.noreply.github.com>
2024-08-27 16:50:58 -0300

    up

---
+++ projetos avançados/teste.js
@@ -0,0 +1,31 @@
+
+let atletas = [
+    {
+      nome: "Cesar Abascal",
+      notas: [10, 9.34, 8.42, 10, 7.88]
+    },
+    {
+      nome: "Fernando Puntel",
+      notas:  [8, 10, 10, 7, 9.33]
+    },
+    {
+      nome: "Daiane Jelinsky",
+      notas: [7, 10, 9.5, 9.5, 8]
+    },
+    {
+      nome: "Bruno Castro",
+      notas: [10, 10, 10, 9, 9.5]
+    }
+   ];
+    
+   for (let i = 0; i < atletas.length; i++) {
+    let atleta = atletas[i];
+    console.log(`Atleta: ${atleta.nome}`);
+    console.log(`Notas obtidas: ${atleta.notas}`);
+}
+
+
+
+
+
+   
