a47c2dfad26df46e7835e7b2820551e6907c7da5
Lucas Marques de Oliveira <167311748+lucasmarqoliv@users.noreply.github.com>
2024-10-30 16:46:44 -0300

    up

--- objetos/metodos_objects.js
+++ objetos/metodos_objects.js
@@ -1,4 +1,6 @@
 // Object.keys(objeto) retorna uma matriz com todas as chaves do objeto. e apartir disso voce pode iterar sobre eles.
+// Object.values(ojt) retorna os valores das chaves
+// Object.entries(obj) retorna as chaves e valores
 
 const pessoa = {
     nome: "Alice",
